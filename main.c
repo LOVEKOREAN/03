94f6239a238c882c9f10c6079d18cedfb71afacb
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-09-21 16:45:08 +0900

    4

--- main.c
+++ main.c
@@ -3,12 +3,21 @@
 
 int main(int argc, char *argv[])
 {
- char c;
- 
- printf("enter a character : ");
- scanf("%c", &c);
- 
- printf("the next character of %c (%d) is %c(%d)\n", c, c, c+1, c+1);
+    
+    int a;
+    int b;
+    
+    printf("enter a:");
+    scanf("%d", &a);
+    
+    printf("enter b:");
+    scanf("%d", &b);
+    
+    
+    
+    printf("result a/b:%f\n", (float)a/b);
+    
+    
  system("PAUSE");	
  return 0;
 }
